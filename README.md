86bb865a27f8a8eaef5c55fc3fd7b34f6ed344a7
kwseeker <xiaohuileee@gmail.com>
2020-04-15 15:59:31 +0800

    temp

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 详细文档参考各子目录下面的Markdown文本。
 
+推荐：
+
+[labuladong/fucking-algorithm](https://github.com/labuladong/fucking-algorithm)
+
+[labuladong的算法小抄](https://labuladong.gitbook.io/algo/)
+
+
+
---
+++ leecode/src/main/java/top/kwseeker/dsaa/leecode/Rectangle542.java
@@ -0,0 +1,39 @@
+package top.kwseeker.dsaa.leecode;
+
+/**
+ * 这其实是一个图的遍历问题
+ * TODO:后面研究完图的遍历思想再完善
+ */
+public class Rectangle542 {
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 0},
+                {1, 1, 1, 0, 0},
+                {1, 1, 1, 0, 0},
+                {1, 1, 1, 0, 0}
+        };
+    }
+
+    //public int[][] updateMatrix(int[][] matrix) {
+    //    int h = matrix[0].length;
+    //    int v = matrix.length;
+    //    int[][] result = new int[v][h];
+    //    for(int i=0; i < v; i++) {
+    //        for(int j=0; j < h; j++) {
+    //            if(matrix[i][j] == 0) {
+    //                result[i][j] = 0;
+    //            } else {
+    //                int count=1;
+    //                for (;;count++) {
+    //                    //获取(j,i)外围菱形所有数据 |x-j|+|y-i|=count
+    //
+    //
+    //                }
+    //                result[i][j] = count;
+    //            }
+    //        }
+    //    }
+    //}
+}
